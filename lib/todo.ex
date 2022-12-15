cda18a47fd4ed3d9ee7e10fd8dafbc64ef6b64f8
divyaaaaa123 <47104889+divyaaaaa123@users.noreply.github.com>
2022-12-15 13:39:42 +0530

    update

--- lib/todo.ex
+++ lib/todo.ex
@@ -6,21 +6,42 @@ defmodule Todo do
     add_task = %Todo.Schema{task: task}
   Todo.Repo.insert(add_task)
   end
+
+  def null() do
+    query = from task in Todo.Schema ,
+          select: task
+    _data=Todo.Repo.all(query)
+    #IO.inspect(data)
+  end
+  def list() do
+      Todo.Repo.insert_all(Todo.Schema,[
+        %{task: "Add New Task " },
+        %{task: "Add New Task " },
+        %{task: "Add New Task " },
+        %{task: "Add New Task " }
+
+      ])
+  end
   def show_task() do
+    if length(null())==0 do
+      list()
+    end
     query = from task in Todo.Schema ,
           select: task
     data=Todo.Repo.all(query)
     tasks = data|> Enum.map(&(&1.task))
-    reverse_tasks=Enum.take(tasks, -4) |> IO.inspect(charlists: :as_lists)
-    IO.inspect(reverse_tasks)
+    _reverse_tasks=Enum.take(tasks, -4)
+
   end
   def show_id() do
+    if length(null())==0 do
+      list()
+    end
     query = from task in Todo.Schema ,
           select: task
     data=Todo.Repo.all(query)
     ids = data|> Enum.map(&(&1.id))
-    reverse_ids=Enum.take(ids, -4) |> IO.inspect(charlists: :as_lists)
-    IO.inspect(reverse_ids)
+    _reverse_ids=Enum.take(ids, -4)
   end
 
 
@@ -30,11 +51,5 @@ defmodule Todo do
 
   end
 
-  # def edit_task(id,task) do
-
-  # end
-  # def delete_task (id) do
-
-  # end
 
 end
